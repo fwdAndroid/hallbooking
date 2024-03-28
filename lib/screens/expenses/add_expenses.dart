559b5fd6194ca8c56504c494277cd5e530416f03
fwdAndroid <fwdkaleem@gmail.com>
2024-03-28 18:22:32 +0500

    fawda

--- lib/screens/expenses/add_expenses.dart
+++ lib/screens/expenses/add_expenses.dart
@@ -22,6 +22,11 @@ class _AddExpensesState extends State<AddExpenses> {
 
   DateTime selectedDate = DateTime.now(); //Start Date
 
+  bool isMobile(BuildContext context) {
+    return MediaQuery.of(context).size.width <
+        600; // Adjust the width breakpoint for mobile
+  }
+
   bool isLoading = false;
   var uuid = Uuid().v4();
 
@@ -60,134 +65,140 @@ class _AddExpensesState extends State<AddExpenses> {
         centerTitle: true,
         title: Text("Add Expenses"),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              margin: const EdgeInsets.only(left: 10, right: 10),
-              child: TextFormInputField(
-                  controller: _nameController,
-                  hintText: "Vendor Name",
-                  IconSuffix: Icons.person,
-                  textInputType: TextInputType.emailAddress),
-            ),
-            Container(
-              margin: const EdgeInsets.only(left: 10, right: 10),
-              child: TextFormInputField(
-                  controller: _contactNumberController,
-                  hintText: "Vendor GST",
-                  IconSuffix: Icons.attach_money,
-                  textInputType: TextInputType.number),
-            ),
-            Container(
-              margin: const EdgeInsets.only(left: 10, right: 10),
-              child: TextFormInputField(
-                  controller: _totalAmountController,
-                  hintText: "Amount",
-                  IconSuffix: Icons.money,
-                  textInputType: TextInputType.number),
-            ),
-            TextFormField(
-              controller: _dateControllerStart,
-              decoration: InputDecoration(
-                labelText: 'Bill Date',
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.calendar_today),
-                  onPressed: () => _selectDate(context),
-                ),
-              ),
-              readOnly: true, // make the text field read-only
-              onTap: () => _selectDate(context), // also open date picker on tap
-            ),
-            Container(
-              margin: const EdgeInsets.only(left: 10, right: 10, top: 10),
-              child: TextFormInputField(
-                  controller: d,
-                  hintText: "Description",
-                  IconSuffix: Icons.telegram,
-                  textInputType: TextInputType.text),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "Category",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: DropdownButton(
-                // Initial Value
-                isExpanded: true,
-                value: dropdownvalue,
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: SingleChildScrollView(
+          child: LayoutBuilder(builder: (context, constraints) {
+            return Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(left: 10, right: 10),
+                    child: TextFormInputField(
+                        controller: _nameController,
+                        hintText: "Vendor Name",
+                        IconSuffix: Icons.person,
+                        textInputType: TextInputType.emailAddress),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 10, right: 10),
+                    child: TextFormInputField(
+                        controller: _contactNumberController,
+                        hintText: "Vendor GST",
+                        IconSuffix: Icons.attach_money,
+                        textInputType: TextInputType.number),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 10, right: 10),
+                    child: TextFormInputField(
+                        controller: _totalAmountController,
+                        hintText: "Amount",
+                        IconSuffix: Icons.money,
+                        textInputType: TextInputType.number),
+                  ),
+                  TextFormField(
+                    controller: _dateControllerStart,
+                    decoration: InputDecoration(
+                      labelText: 'Bill Date',
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.calendar_today),
+                        onPressed: () => _selectDate(context),
+                      ),
+                    ),
+                    readOnly: true, // make the text field read-only
+                    onTap: () =>
+                        _selectDate(context), // also open date picker on tap
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 10, right: 10, top: 10),
+                    child: TextFormInputField(
+                        controller: d,
+                        hintText: "Description",
+                        IconSuffix: Icons.telegram,
+                        textInputType: TextInputType.text),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: DropdownButton(
+                      // Initial Value
+                      isExpanded: true,
+                      value: dropdownvalue,
 
-                // Down Arrow Icon
-                icon: const Icon(Icons.keyboard_arrow_down),
+                      // Down Arrow Icon
+                      icon: const Icon(Icons.keyboard_arrow_down),
 
-                // Array list of items
-                items: items.map((String items) {
-                  return DropdownMenuItem(
-                    value: items,
-                    child: Text(items),
-                  );
-                }).toList(),
-                // After selecting the desired option,it will
-                // change button value to selected value
-                onChanged: (String? newValue) {
-                  setState(() {
-                    dropdownvalue = newValue!;
-                  });
-                },
-              ),
-            ),
-            isLoading
-                ? Center(child: CircularProgressIndicator())
-                : Center(
-                    child: SaveButton(
-                      onTap: () async {
-                        if (_nameController.text.isEmpty ||
-                            _contactNumberController.text.isEmpty ||
-                            _totalAmountController.text.isEmpty ||
-                            _dateControllerStart.text.isEmpty) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(content: Text("All Fields Required")));
-                        } else {
-                          setState(() {
-                            isLoading = true;
-                          });
-                          await FirebaseFirestore.instance
-                              .collection("expenses")
-                              .doc(uuid)
-                              .set({
-                            "uuid": uuid,
-                            "vendorName": _nameController.text,
-                            "vendorGST":
-                                int.parse(_contactNumberController.text), //GST
-
-                            "amount": int.parse(_totalAmountController.text),
-
-                            "billDate": _dateControllerStart.text,
-                            "description": d.text,
-
-                            "category": dropdownvalue,
-                          });
-
-                          setState(() {
-                            isLoading = false;
-                          });
-                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                              content: Text("Expense Successfully Added")));
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (builder) => MainDashboard()));
-                        }
+                      // Array list of items
+                      items: items.map((String items) {
+                        return DropdownMenuItem(
+                          value: items,
+                          child: Text(items),
+                        );
+                      }).toList(),
+                      // After selecting the desired option,it will
+                      // change button value to selected value
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          dropdownvalue = newValue!;
+                        });
                       },
-                      title: "Save",
                     ),
                   ),
-          ],
+                  isLoading
+                      ? Center(child: CircularProgressIndicator())
+                      : Center(
+                          child: SaveButton(
+                            onTap: () async {
+                              if (_nameController.text.isEmpty ||
+                                  _contactNumberController.text.isEmpty ||
+                                  _totalAmountController.text.isEmpty ||
+                                  _dateControllerStart.text.isEmpty) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                        content: Text("All Fields Required")));
+                              } else {
+                                setState(() {
+                                  isLoading = true;
+                                });
+                                await FirebaseFirestore.instance
+                                    .collection("expenses")
+                                    .doc(uuid)
+                                    .set({
+                                  "uuid": uuid,
+                                  "vendorName": _nameController.text,
+                                  "vendorGST": int.parse(
+                                      _contactNumberController.text), //GST
+
+                                  "amount":
+                                      int.parse(_totalAmountController.text),
+
+                                  "billDate": _dateControllerStart.text,
+                                  "description": d.text,
+
+                                  "category": dropdownvalue,
+                                });
+
+                                setState(() {
+                                  isLoading = false;
+                                });
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                        content: Text(
+                                            "Expense Successfully Added")));
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (builder) => MainDashboard()));
+                              }
+                            },
+                            title: "Save",
+                          ),
+                        ),
+                ],
+              ),
+            );
+          }),
         ),
       ),
     );
